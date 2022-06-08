960912c9cfb2173c12de40772f392a8574111b30
Ari Schermuk <arielschermuk@gmail.com>
2022-06-08 19:36:27 -0300

    remove unused 'next' params

--- src/group/controller/groupController.js
+++ src/group/controller/groupController.js
@@ -4,19 +4,19 @@ export const getGroupsController = (req, res) => {
     res.json(getGroups())
 }
 
-export const getGroupController = (req, res, next) => {
+export const getGroupController = (req, res) => {
     res.json(getGroup(req.params.id))
 }
 
-export const addGroupController = (req, res, next) => {
+export const addGroupController = (req, res) => {
     res.status(201).json(addGroup(req.body))
 }
 
 
-export const updateGroupController = (req, res, next) => {
+export const updateGroupController = (req, res) => {
     res.json(replaceGroup(req.params.id, req.body))
 }
 
-export const deleteGroupController = (req, res, next) => {
+export const deleteGroupController = (req, res) => {
     res.json(removeGroup(req.params.id))
 }
--- src/participant/controller/participantController.js
+++ src/participant/controller/participantController.js
@@ -4,19 +4,19 @@ export const getParticipantsController = (req, res) => {
     res.json(getParticipants())
 }
 
-export const getParticipantController = (req, res, next) => {
+export const getParticipantController = (req, res) => {
     res.json(getParticipant(req.params.id))
 }
 
-export const addParticipantController = (req, res, next) => {
+export const addParticipantController = (req, res) => {
     res.status(201).json(addParticipant(req.body))
 }
 
 
-export const updateParticipantController = (req, res, next) => {
+export const updateParticipantController = (req, res) => {
     res.json(replaceParticipant(req.params.id, req.body))
 }
 
-export const deleteParticipantController = (req, res, next) => {
+export const deleteParticipantController = (req, res) => {
     res.json(removeParticipant(req.params.id))
 }
